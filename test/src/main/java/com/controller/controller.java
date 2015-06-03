eaf21004a293e8a50ba8fc84536411f6b5e827cc
MoonWoong <zzgkwkd@gmail.com>
2015-06-03 14:38:40 +0900

    dd

--- test/src/main/java/com/controller/controller.java
+++ test/src/main/java/com/controller/controller.java
@@ -1,12 +1,10 @@
 package com.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-public class Controller {
+@Controller
+public class controller {
 
 	@RequestMapping("/")
 	public String index(){
